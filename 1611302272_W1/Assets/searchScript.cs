1d44c3ad64cb29d4a6f67ef086ea3dabe8cc7be3
kana-scyGIT <kankan2543.21@gmail.com>
2021-01-24 16:34:21 +0700

    First version (Incomplete)

---
+++ 1611302272_W1/Assets/searchScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class searchScript : MonoBehaviour
+{
+    public InputField textField;
+    public static string inputText;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        inputText = textField.text;
+    }
+    
+    
+}
